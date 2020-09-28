f08413e701d7d96ebadd2f343076ae2a86fbd1c0
German Altairac <saltairac@uade.edu.ar>
2020-09-28 00:10:49 -0300

    added age calculation
    forgot whoopsie

--- js/scripts.js
+++ js/scripts.js
@@ -1,8 +1,18 @@
 $(document).ready(function (){
     $("#header").load("./html/header.html");
     $("#footer").load("./html/footer.html");
-    console.log("test")
+    $("#age").html(calculateCurrentAge());
+
 });
 $(window).scroll(function(){
     $("#arrows").css("opacity", 1 - $(window).scrollTop() / 250);
 });
+
+
+function calculateCurrentAge(){
+    const dateOfBirth = new Date(1998, 1, 21);
+    const differenceInMs = Date.now() - dateOfBirth.getTime();
+    const calculatedDate = new Date(differenceInMs);
+
+    return Math.abs(calculatedDate.getUTCFullYear() - 1970);
+}
