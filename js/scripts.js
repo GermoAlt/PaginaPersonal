bf2257615caf7ddb8c3f6f71e8f94841084d9822
German Altairac <saltairac@uade.edu.ar>
2020-09-28 00:47:51 -0300

    refresh banners for chrome

--- js/scripts.js
+++ js/scripts.js
@@ -1,13 +1,19 @@
 $(document).ready(function (){
     $("#header").load("./html/header.html");
     $("#footer").load("./html/footer.html");
+    window.setTimeout(refreshBanners, 40)
     $("#age").html(calculateCurrentAge());
-
 });
+
 $(window).scroll(function(){
     $("#arrows").css("opacity", 1 - $(window).scrollTop() / 250);
 });
 
+function refreshBanners(){
+    const banners = $(".banner");
+    banners.hide();
+    banners.show();
+}
 
 function calculateCurrentAge(){
     const dateOfBirth = new Date(1998, 1, 21);
