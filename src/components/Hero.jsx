1f64a552f522c92329624b788cdfcffb25c6d0dd
Germán Altairac <germanaltairac@hotmail.com>
2023-08-21 22:45:53 -0300

    projects

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -46,7 +46,7 @@ const Hero = () => {
               </span>
             </h1>
             <p className={`${styles.heroSubText} mt-2 text-timberWolf`}>
-              I like to build cool stuff<br className="sm:block hidden" />
+              I like to build cool stuff <br className="sm:block hidden" />
               and help others do the same.
             </p>
           </div>
--- src/constants/index.js
+++ src/constants/index.js
@@ -119,147 +119,106 @@ const technologies = [
 
 const experiences = [
   {
-    title: 'Front-End Developer',
-    company_name: 'Cover Hunt',
+    title: 'Full Stack Developer',
+    company_name: 'Freelance',
     icon: coverhunt,
-    iconBg: '#333333',
-    date: 'Aug 2021 - Feb 2022',
-  },
-  {
-    title: 'Mentor (Volunteer)',
-    company_name: 'Microverse',
-    icon: microverse,
-    iconBg: '#333333',
-    date: 'Mar 2022 - May 2022',
-  },
-  {
-    title: 'Junior Software Engineer',
-    company_name: 'Kelhel',
-    icon: kelhel,
-    iconBg: '#333333',
-    date: 'May 2022 - Oct 2022',
+    date: 'Apr 2016 - Feb 2023',
+    description: "Website development for individual clients.",
+    tech: "Gradle · ReactJS · HTML · JavaScript · CSS · WordPress"
   },
   {
     title: 'Full Stack Developer',
-    company_name: 'Diversity Cyber Council',
+    company_name: 'CDA Informatica',
+    icon: microverse,
+    date: 'Nov 2018 - Nov 2019',
+    description:"I was assigned to the development of technological solutions for the client Banco Galicia, where I helped in the construction of internal workflow management tools for the processing of credit applications for large companies. After it went into production, I was in charge of its maintenance and incident management.",
+    tech:"SOAP · Java · SQL · Hibernate · JavaServer Faces (JSF)"
+  },
+  {
+    title: 'Full Stack Developer',
+    company_name: 'Colonia Suiza',
+    icon: kelhel,
+    date: 'Nov 2019 - Apr 2020',
+    description:"Development and maintenance of clinic management system with billing to AFIP (Argentina's national revenue service), continuous improvement work.",
+    tech:"REST APIs · JPA · Java · SQL · JavaScript · Struts 2"
+  },
+  {
+    title: 'Full Stack Developer',
+    company_name: 'Baufest',
     icon: dcc,
-    iconBg: '#333333',
-    date: 'Sep 2022 - Present',
+    date: 'Apr 2020 - Apr 2021',
+    description:"I was part of the development team assigned to the Warner Media client, where we worked on building new infrastructure solutions and in the iterative process of continuous improvement of existing software.",
+    tech:"Gradle · Angular · Spring Integration · Spring Boot · REST APIs · Java"
+  },
+  {
+    title: 'Innovation Lead',
+    company_name: 'AssA Alimentos del Sur',
+    icon: dcc,
+    date: 'Feb 2022 - Oct 2022',
+    description:"In charge of designing, building and implementing an internal inventory tracking system, with package traceability via Android mobile app.",
+    tech:"Gradle · Android · DevOps · Automation · Java"
+  },
+  {
+    title: 'Director',
+    company_name: 'Buffer Project',
+    icon: dcc,
+    date: 'Aug 2018 - Feb 2023',
+    description:"Non-profit initiative that functions as an incubator for IT professionals by creating a space for collaboration and learning. As director, I was responsible for the daily project management activities, such as the assembly and maintenance of the technological infrastructure and team management. In addition, I acted as a teacher if necessary.",
+    tech:"Gradle · Web Design · React.js · REST APIs · Java · SQL"
+  },
+  {
+    title: 'Senior Technical Consultant',
+    company_name: 'Kin + Carta',
+    icon: dcc,
+    date: 'Mar 2023 - Present',
+    description:"",
+    tech:""
   },
 ];
 
 const projects = [
   {
     id: 'project-1',
-    name: 'Sitio GESI',
-    description: 'A comic characters list app that displays Marvel characters.',
-    tags: [
-      {
-        name: 'react',
-        color: 'blue-text-gradient',
-      },
-      {
-        name: 'mongodb',
-        color: 'green-text-gradient',
-      },
-      {
-        name: 'tailwind',
-        color: 'pink-text-gradient',
-      },
-    ],
+    name: 'GESI.org.ar',
+    description: 'Institutional website for GESI built with ReactJS.',
     image: komikult,
-    repo: 'https://github.com/shaqdeff/KomiKult',
-    demo: 'https://shaqdeff.github.io/KomiKult/',
+    repo: 'https://github.com/GermoAlt/sitio-gesi',
+    demo: 'https://www.gesi.org.ar/',
   },
   {
     id: 'project-2',
     name: 'Lorena',
     description:
-      'A leaderboard list app that displays scores submitted by different players.',
-    tags: [
-      {
-        name: 'react',
-        color: 'blue-text-gradient',
-      },
-      {
-        name: 'restapi',
-        color: 'green-text-gradient',
-      },
-      {
-        name: 'scss',
-        color: 'pink-text-gradient',
-      },
-    ],
+      'A discord bot built with Java/Kotlin that provides a \"hall of fame\" ' +
+        'of messages that users store via emoji reactions. ' +
+        'It also provides automatic/on demand unit conversions.',
     image: leaderboard,
-    repo: 'https://github.com/shaqdeff/Leaderboard',
-    demo: 'https://shaqdeff.github.io/Leaderboard/',
+    repo: 'https://github.com/Buffer-Project/Lorena',
   },
   {
     id: 'project-3',
     name: 'Barcode Scanner',
-    description: 'This is a single-page calculator app built with React',
-    tags: [
-      {
-        name: 'nextjs',
-        color: 'blue-text-gradient',
-      },
-      {
-        name: 'supabase',
-        color: 'green-text-gradient',
-      },
-      {
-        name: 'css',
-        color: 'pink-text-gradient',
-      },
-    ],
+    description: 'Android native app that scans barcodes for warehouse inventory tracking.',
     image: math,
-    repo: 'https://github.com/shaqdeff/Math-Magicians',
-    demo: 'https://inspiring-medovik-37d3b3.netlify.app/',
+    repo: 'https://github.com/GermoAlt/BarcodeScanner',
   },
   {
     id: 'project-4',
-    name: 'Gourmetic',
-    description: `A single-page application that allows users to search for any movie or show's ratings and its details.`,
-    tags: [
-      {
-        name: 'nextjs',
-        color: 'blue-text-gradient',
-      },
-      {
-        name: 'supabase',
-        color: 'green-text-gradient',
-      },
-      {
-        name: 'css',
-        color: 'pink-text-gradient',
-      },
-    ],
+    name: 'SQL Injection',
+    description: `Educational site to demonstrate the mechanics of SQL injection built with ReactJS 
+    and an in-memory SQLite database.`,
     image: movie,
-    repo: 'https://github.com/shaqdeff/Movie-Metro',
-    demo: 'https://movie-metro.netlify.app/',
+    repo: 'https://github.com/GermoAlt/sql-injection-demo',
+    demo: 'https://sql-spa-injection-demo-3b3a23d88bb2.herokuapp.com/',
   },
   {
     id: 'project-5',
     name: 'iRent',
     description:
-      'This is a demo concert website for a music festival called Nyeusi.',
-    tags: [
-      {
-        name: 'nextjs',
-        color: 'blue-text-gradient',
-      },
-      {
-        name: 'supabase',
-        color: 'green-text-gradient',
-      },
-      {
-        name: 'css',
-        color: 'pink-text-gradient',
-      },
-    ],
+      'Demo site for an e-commerce platform built with ReactJS',
     image: nyeusi,
-    repo: 'https://github.com/shaqdeff/Nyeusi-Fest-Site',
-    demo: 'https://shaqdeff.github.io/Nyeusi-Fest-Site/',
+    repo: 'https://github.com/GermoAlt/iRent_front',
+    demo: 'https://i-rent-front.vercel.app/',
   },
 ];
 
